d159f751bc377475ba287b1e411bd2aa4681e3c0
Sam <lunar_sam@protonmail.com>
2023-07-15 12:13:09 -0400

    sam is writing cpio stuff again

---
+++ luna/libs/cpio.lua
@@ -0,0 +1,20 @@
+local cpio_read = {}
+local cpio_write = {}
+
+local hdr = ""
+
+ function cpio_write:add_entry(stat)
+
+ end
+
+ function cpio_write:writedata(stat)
+
+ end
+
+ function cpio_write:align()
+
+ end
+
+ function cpio_read:read_file()
+
+ end
--- luna_modules/build.lua
+++
@@ -1,3 +0,0 @@
-local has_cpio = os.execute("cpio --version")
-local has_7z = os.execute("7z")
-
